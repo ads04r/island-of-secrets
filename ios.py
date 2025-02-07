78c8056f3ff49644e8f59c2eff2d3ad0bfbe96ab
Ash <ash@drashsmith.com>
2025-02-07 00:42:31 +0000

    Basic parser

--- ios.py
+++ ios.py
@@ -26,14 +26,65 @@ class Game():
 		self.CONST_C1 = 16
 		self.CONST_C2 = 21
 		self.CONST_C3 = 24
+		self.CONST_C4 = 43
+
+	def items_seen(self):
+
+		items = []
+		for i in range(0, self.CONST_C4):
+			if ((self.items[i][2] == self.location) & (self.items[i][3] < 1)):
+				items.append(items[i])
+		return items
+
+	def prose(self):
+
+		items = self.items_seen()
+		location = self.locations[self.location - 1]
+
+		prose = ["ISLAND OF SECRETS", "-----------------", "Time remaining: " + str(self.time_remaining), '']
+		prose.append("You are " + (self.prepositions[location[0] - 1].lower()) + " " + location[1])
+		if len(items) > 0:
+			prose.append("You see: " + (', '.join([x[1] for x in items])))
+		prose.append('')
+		prose.append(self.status)
+		return prose
+
+	def __lookup_word(self, word):
+
+		vc = len(self.verbs)
+		nc = len(self.items)
+		for i in range(0, nc):
+			if word.startswith(self.items[i][0]):
+				return [False, i + 1]
+		for i in range(0, vc):
+			if word.startswith(self.verbs[vc - (i + 1)]):
+				return [True, vc - i]
+		return []
+
+	def __parse(self, text):
+
+		nouns = []
+		verbs = []
+		for word in text.strip().split(' '):
+			item = self.__lookup_word(word.upper())
+			if len(item) != 2:
+				continue
+			if item[0]:
+				verbs.append(item[1])
+			else:
+				nouns.append(item[1])
+		return [self.verbs[x - 1] for x in verbs], [self.items[x - 1] for x in nouns]
 
 	def input(self, text):
 
+		v, n = self.__parse(text)
+		if 'QUI' in v:
+			self.over = True
 		return
 
 if __name__ == "__main__":
 
 	game = Game()
 	while not game.over:
-		print(game.status)
+		print('\n'.join(game.prose()))
 		game.input(input().upper())
